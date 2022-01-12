ddc5b7a4407635518c2aa9d973c3ff3e69c9b7d9
Ramil Galikeev <galikeevramil95@gmail.com>
2022-01-12 11:10:42 +0500

    added hover effect for links and buttons

--- src/components/app-navigation/app-navigation.js
+++ src/components/app-navigation/app-navigation.js
@@ -7,24 +7,44 @@ import './app-navigation.scss';
 
 const AppNavigation = (props) => {
     
-    const colorLink = props.footer ? {color: '#000000'} : {color: '#ffffff'};
+    const colorLink = props.footer ? "navigation__links_black" : "navigation__links_white";
     const colorLogo = props.footer ? LogoBlack : LogoWhite;
     const menuPosition = props.footer ? {justifyContent: 'center'} : {justifyContent: 'flex-start'}
 
     return (
         <nav className='navigation'>
             <ul className="navigation__menu" style={menuPosition}>
+                <NavLink
+                    end
+                    to="/">
+                    <img src={colorLogo} alt="logo" />
+                </NavLink>
                 <li className="navigation__links">
-                    <NavLink end style={colorLink} to="/">
-                        <img src={colorLogo} alt="logo" />
+                    <NavLink 
+                        end
+                        style={({isActive}) => ({color: isActive ? '#9f0013' : null})}
+                        to="/"
+                        className={colorLink}>
                         Coffee house
                     </NavLink>
                 </li>
                 <li className="navigation__links">
-                    <NavLink end style={colorLink} to="/our-coffee">Our coffee</NavLink>
+                    <NavLink 
+                        end
+                        style={({isActive}) => ({color: isActive ? '#9f0013' : null})}
+                        to="/our-coffee"
+                        className={colorLink}>
+                            Our coffee
+                    </NavLink>
                 </li>
                 <li className="navigation__links">
-                    <NavLink end style={colorLink} to="/for-your-pleasure">For your pleasure</NavLink>
+                    <NavLink 
+                        end 
+                        style={({isActive}) => ({color: isActive ? '#9f0013' : null})} 
+                        to="/for-your-pleasure"
+                        className={colorLink}>
+                            For your pleasure
+                    </NavLink>
                 </li>
             </ul>
         </nav>
